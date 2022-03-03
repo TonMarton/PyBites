6884673bf5789fe2793657bddc2d2b780efeb7ff
Márton Nagy <ton.marton@gmail.com>
2022-03-03 08:25:50 -0500

    added smarter and official solution for bite 29

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -27,6 +27,20 @@ def get_index_different_char(chars: list):
                 if str(chars[i]).isalnum():
                     return i
 
-    
+    import string 
 
-        
+    alphanumeric_chars = list(string.ascii_letters + string.digits)
+    
+    def get_index_different_char_official(chars):
+        matches, no_matches = [], []
+        for i, char in enumerate(chars):
+            if str(char).lower() in alphanumeric_chars:
+                matches.append(i)
+        else:
+                no_matches.append(i)
+        return matches[0] if len(matches) == 1 else no_matches[0]
+
+    def get_index_different_char_smart(chars):
+        bools = [str(c).isalnum() for c in chars]
+        return bools.index(True) if bools.count(True) == 1 else bools.index(False)
+    

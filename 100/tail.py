ea4983f7dfda0e9cb3fdcae0928d0f2b74dc02f2
Márton Nagy <ton.marton@gmail.com>
2022-03-04 09:35:10 -0500

    bite 100 improvement

--- 100/tail.py
+++ 100/tail.py
@@ -2,6 +2,4 @@ def tail(filepath, n):
    """Similate Unix' tail -n, read in filepath, parse it into a list,
        strip newlines and return a list of the last n lines"""
    with open(filepath, 'r', ) as f:
-      last_n_lines = f.read().splitlines()[-n:]
-      return [line.strip('\n') for line in last_n_lines]
-
+      return f.read().splitlines()[-n:]
